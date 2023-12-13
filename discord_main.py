643b67d390016df27d927a7f89cee37d87a630ff
Mr. Kaan <149811898+kcorrai@users.noreply.github.com>
2023-12-13 21:56:11 +0300

    Update discord_main.py

--- discord_main.py
+++ discord_main.py
@@ -3,24 +3,27 @@ import discord_token
 from discord.ext import commands
 import requests
 import random
-from movies import dovus_filmleri, korku_filmleri, romantik_filmler
+from movies import dovus_filmleri, korku_filmleri, romantik_filmler, testler
 
 intents = discord.Intents.default()
 intents.message_content = True
 
-bot = commands.Bot(command_prefix='!', intents=intents) #help_command=None
+bot = commands.Bot(command_prefix='!', intents=intents, help_command=None) #help_command=None
 
 @bot.event
 async def on_ready():
     print(f'{bot.user} olarak giriş yaptık')
     
 @bot.command()
-async def help2(ctx):
-    embed = discord.Embed(title="Bot Komutları", description="Bu bot filmlerle alakalı", color=discord.Color.green())
+async def help(ctx):
+    embed = discord.Embed(title="Bot Komutları", color=discord.Color.green())
+    embed.add_field(name="!filmöner", value="Sana sevdiğin film türlerine göre film tavsiyesinde bulunur.", inline=False)
+    embed.add_field(name="!filmara", value="Aramak istediğin filmin konusunu, inceleme puanını, posterlerini vs. gösterir. NOT: İşlemi çağırdıktan sonra aranmak istenen film adı yazılmalıdır")
+    embed.add_field(name="!filmtesti", value="Sana film bilgilerini test edebilecek birçok farklı web sitesi gösterir.")
     await ctx.send(embed=embed)
 
 @bot.command()
-async def oneri(ctx):
+async def filmöner(ctx):
     await ctx.send("Ne tür filmleri seversin? (dövüş, romantık, korku) ")
     while True:
         try:
@@ -87,22 +90,31 @@ API_KEY = 'api key from OMDb API'
 
 def search_movie(movie_name):
     url = "https://www.omdbapi.com/?t={}".format(movie_name)+f"&apikey={API_KEY}"
+    print(url)
     response = requests.get(url)
     if response.status_code == 200:
         data = response.json()
-        story_info = data["Plot"]
-        return story_info
+        movie_plot = data["Plot"]
+        movie_rating = data["imdbRating"]
+        movie_poster = data["Poster"]
+        return [movie_plot, movie_rating, movie_poster]
     else:
         return None
     
 @bot.command()
-async def story(ctx, *movie_name):
+async def filmara(ctx, *movie_name):
     full_movie_name = " ".join(movie_name)
     story_info = search_movie(full_movie_name)
     if story_info is not None:
-        await ctx.send("Verilen filmin hikaye taslağı '{}' is: {}".format(full_movie_name, story_info))
+        await ctx.send("Verilen filmin hikaye taslağı '{}' is: {}".format(full_movie_name, story_info[0]))
+        await ctx.send(f"IMDb Puanı: {story_info[1]}")
+        await ctx.send(story_info[2])
     else:
-        await ctx.send("Film için hikaye konusu bulunamadı '{}'".format(full_movie_name))
-    
+        await ctx.send("Film bulunamadı '{}'".format(full_movie_name))
+        
+@bot.command()
+async def filmtesti(ctx):
+    link, aciklama = random.choice(list(testler.items()))
+    await ctx.send(f"{aciklama} => {link}")    
         
 bot.run(discord_token.token)
